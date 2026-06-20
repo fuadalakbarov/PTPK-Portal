52b9465a124e96170e6aa934fd5b26c13399a3e3
Fuad Alakbarov <115851305+fuadalakbarov@users.noreply.github.com>
2026-06-21 00:06:13 +0400

    Update tailwind.config.js

--- PTPK-Portal/PTPK-Portal/PTPK-Portal/tailwind.config.js
+++ PTPK-Portal/PTPK-Portal/PTPK-Portal/tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        navy: {
+          DEFAULT: '#0d1b2a',
+          light: '#1a2e45',
+        },
+        gold: {
+          DEFAULT: '#c9a84c',
+          light: '#f0d080',
+        },
+        teal: {
+          DEFAULT: '#0f6e6e',
+        },
+      },
+      fontFamily: {
+        serif: ['"Noto Serif"', 'serif'],
+        sans: ['"DM Sans"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
